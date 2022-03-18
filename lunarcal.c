ef848f24fd01cd085ec70ef6d3e0e562e20444ae
ldm2060 <ldm2060@outlook.com>
2022-03-18 19:59:57 +0800

    Update lunarcal.c

--- lunarcal.c
+++ lunarcal.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     int start, end;
     time_t now = time(0);
-	tm *ltm = localtime(&now);
+	struct tm *ltm = localtime(&now);
 	end = 1900 + ltm->tm_year + 1;
 	start = end - 2;
 
@@ -17,9 +17,9 @@ int main(int argc, char *argv[])
            "VERSION:2.0\n"
            "CALSCALE:GREGORIAN\n"
            "METHOD:PUBLISH\n"
-           "X-WR-CALNAME:24节气\n"
+           "X-WR-CALNAME:24陆脷脝酶\n"
            "X-WR-TIMEZONE:Asia/Shanghai\n"
-           "X-WR-CALDESC:中国农历%d-%d, 包括节气.\n", start, end);
+           "X-WR-CALDESC:脰脨鹿煤脜漏脌煤%d-%d, 掳眉脌篓陆脷脝酶.\n", start, end);
     while (start <= end) {
         cn_lunarcal(start);
         start++;
